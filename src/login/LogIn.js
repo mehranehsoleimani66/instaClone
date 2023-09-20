2cb87a4882e896e7a4e63b37452be76884106af9
mehraneh <139059903+mehranehsoleimani66@users.noreply.github.com>
2023-09-20 18:49:07 +0330

    Create LogIn.js

---
+++ src/login/LogIn.js
@@ -0,0 +1,54 @@
+import appStore from '../assets/appStore.png'
+import googlePlay from '../assets/googlePlay.png'
+import logo from '../assets/logo.png'
+import faceBook from '../assets/faceBook.png'
+import '../App.css'
+import './login.css'
+const LogIn = () => {
+    return ( 
+        <div className="login_container">
+            <div className="box_1">
+             
+                <img src={logo} alt='' className='instagram_logo'/>
+               
+               
+                <div className='input_box'>
+                    <input  type='text' placeholder='Phone Number, UserName,Email Adress'/>
+                </div>
+                <div className='input_box'>
+                    <input type='password'  />
+                </div>
+                <div className='login_button_box'>
+                    <button className='login_button'>Log IN</button>
+                </div>
+                <div className='lines_box'>
+                    <div className='line_1'></div>
+                    <div className='or'>oR</div>
+                    <div className='line_1'></div>
+                </div>
+                <div className='fb_link'>
+                    <span>
+                        <img src={faceBook} alt='#' className='fb_logo'/>
+                    </span>
+                    <p className='fb-link'>Log In With Facebook</p>
+                </div>
+                <div className='forgatten_password_box'>
+                    <p className='forgatten_password_link'>Forgatten Your Password</p>
+                    </div>
+            </div>
+            <div className="box_2">
+                <p>dont have an account?<span>sign in</span></p>
+            </div>
+            <div className="get_app_box">
+                <p>Get the APP</p>
+            </div>
+            <div className="app_store_google_play">
+            <img src={appStore} alt='' className='appStore'/>
+            <img src={googlePlay} alt='' className='googlePlay'/>
+            </div>
+
+        </div>
+     );
+}
+ 
+export default LogIn;
