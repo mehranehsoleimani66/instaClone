11a4eaf7bd7e715195e54ced8012708a0b5eea64
mehraneh <139059903+mehranehsoleimani66@users.noreply.github.com>
2023-09-20 16:00:05 +0330

    create suggestion component  with css

--- src/timeLine/Suggestion.js
+++ src/timeLine/Suggestion.js
@@ -1,9 +1,55 @@
 import React from 'react'
 import './suggestion.css'
+import { Avatar } from '@mui/material'
 function Suggestion() {
   return (
-    <div>
-      suggestuio
+    <div className='suggestion'>
+<div className='suggestion_title'>Suggestion for you
+</div>
+<div className='suggestion_users'>
+  <div className='suggestion_usernameLeft'>
+    <span className='avatar'>
+    <Avatar>R</Avatar>
+    </span>
+    <div className='username_info'>
+    <span className='username'>radin</span>
+    <span className='relation'>new in instagram</span>
+    </div>
+    
+  </div>
+
+<button className='follow_button'>Follow</button>
+    </div>
+
+    <div className='suggestion_users'>
+  <div className='suggestion_usernameLeft'>
+    <span className='avatar'>
+    <Avatar>R</Avatar>
+    </span>
+    <div className='username_info'>
+    <span className='username'>radin</span>
+    <span className='relation'>new in instagram</span>
+    </div>
+    
+  </div>
+
+<button className='follow_button'>Follow</button>
+    </div>
+
+    <div className='suggestion_users'>
+  <div className='suggestion_usernameLeft'>
+    <span className='avatar'>
+    <Avatar>R</Avatar>
+    </span>
+    <div className='username_info'>
+    <span className='username'>radin</span>
+    <span className='relation'>new in instagram</span>
+    </div>
+    
+  </div>
+
+<button className='follow_button'>Follow</button>
+    </div>
     </div>
   )
 }
