74c50f58239d4e5fc201d49b64da1aac42a166bc
mehraneh <139059903+mehranehsoleimani66@users.noreply.github.com>
2023-09-20 16:00:35 +0330

    import post.js in tileline.js

--- src/timeLine/TimeLine.js
+++ src/timeLine/TimeLine.js
@@ -1,13 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './timeline.css'
 import Suggestion from './Suggestion';
 import Post from './Post';
-function timeLine() {
+const TimeLine=()=> {
+  const [posts,setPost]=useState([
+    {
+      id: 0,
+      user:'marriam_34',
+      postImg:'./pic2.png',
+      likes:'128',
+      timespamp:'12h'
+
+    },
+    {id:1 ,
+      user:'raddin_.',
+      postImg:'./pic1.png',
+      likes:'128',
+      timespamp:'1min'},
+      {
+        id: 2,
+      user:'maxiee2098',
+      postImg:'./pic3.png',
+      likes:'1098',
+      timespamp:'3h'
+
+      }
+
+  ])
   return (
     <div className='timeline'>
      <div className='timeline_left'>
       <div className='timeline_post'>
-        <Post/>
+        {posts.map((item)=>(
+          <Post 
+          key={item.id}
+          user={item.user}
+          postImg={item.postImg}
+          likes={item.likes}
+          timespamp={item.timespamp}
+          />
+          
+        )
+          
+        )}
+       
+
       </div>
      </div>
      <div className='timeline_right'>
@@ -17,4 +54,4 @@ function timeLine() {
   )
 }
 
-export default timeLine;
+export default TimeLine;
