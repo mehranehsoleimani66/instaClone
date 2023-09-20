633529996428aa0e217c800c426833349d5f4f91
mehraneh <139059903+mehranehsoleimani66@users.noreply.github.com>
2023-09-20 18:50:51 +0330

    add login component in app component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 
+import { Route,Routes } from 'react-router';
 import './App.css';
 import HomePage from './HomePage';
+import LogIn from './login/LogIn';
 
 
 function App() {
   return (
     <div className='App'>
-      <HomePage/>
+      <LogIn/>
+{/* <Routes>
+     <Route index element={<HomePage/>} />
+     <Route path='/Shop' element={<LogIn />}/>
+     
+   
+      </Routes>
+       */}
+    
+      
     </div>
   )
 }
