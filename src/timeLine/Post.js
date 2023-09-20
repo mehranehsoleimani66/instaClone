a74f605899f062492a6d587f641107a2e6c81fba
mehraneh <139059903+mehranehsoleimani66@users.noreply.github.com>
2023-09-20 15:59:19 +0330

    create post.js

--- src/timeLine/Post.js
+++ src/timeLine/Post.js
@@ -1,20 +1,39 @@
 import React from 'react'
 import './Post.css'
 import {Avatar} from '@mui/material';
-import  { MoreHorizOutlined } from '@mui/icons-material';
-function Post() {
+import ChatIcon from '@mui/icons-material/Chat';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import TelegramIcon from '@mui/icons-material/Telegram';
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
+import BookmarkBorderIcon from '@mui/icons-material/BookmarkBorder';
+const Post=({user,postImg,likes,timespamp,id})=> {
+  
   return (
     <div className='post'>
      <div className='post_header'>
-     <div className='post_header'>
+     <div className='post_headerAuthor'>
      <Avatar>R</Avatar>
-        radin_.<span>12h</span>
+        {user}<span>{timespamp}</span>
      </div>
-     <MoreHorizOutlined/>
+     <MoreHorizIcon/>
      </div>
   
-     <div className='post-image'></div>
-     <div className='post-footer'></div>
+     <div className='post-image'>
+      <img src={postImg} alt=''/>
+     </div>
+     <div className='post-footer'>
+      <div className='post_footerIcons'>
+        <div className='post_footerMainIcons'>
+          <FavoriteBorderIcon className='post_icon'/>
+          <ChatIcon className='post_icon'/>
+          <TelegramIcon className='post_icon'/>
+        </div>
+        <div className='post_footerSaveIcon'>
+          <BookmarkBorderIcon/>
+        </div>
+      </div>
+      Liked by {likes} people
+     </div>
     </div>
   )
 }
