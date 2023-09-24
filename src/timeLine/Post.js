f8855928c3f8297336688c26292a8847c45be593
mehraneh <139059903+mehranehsoleimani66@users.noreply.github.com>
2023-09-24 16:33:58 +0330

    update post.js

--- src/timeLine/Post.js
+++ src/timeLine/Post.js
@@ -6,20 +6,21 @@ import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import TelegramIcon from '@mui/icons-material/Telegram';
 import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
 import BookmarkBorderIcon from '@mui/icons-material/BookmarkBorder';
-const Post=({user,postImg,likes,timespamp,id})=> {
+const Post=({userName,userAvatar,likesCount,commentText,postImage,postTitle,postBody,timespamp})=> {
   
   return (
     <div className='post'>
      <div className='post_header'>
      <div className='post_headerAuthor'>
-     <Avatar>R</Avatar>
-        {user}<span>{timespamp}</span>
+     {/* <Avatar >{userAvatar}</Avatar> */}
+     <img src={userAvatar} style={{width:'50px', height:'50px', borderRadius:'50px',paddingRight:'5px'}} alt="User Avatar"/>
+        {userName}<span>{timespamp}</span>
      </div>
      <MoreHorizIcon/>
      </div>
   
      <div className='post-image'>
-      <img src={postImg} alt=''/>
+      <img src={postImage} alt=''/>
      </div>
      <div className='post-footer'>
       <div className='post_footerIcons'>
@@ -32,7 +33,8 @@ const Post=({user,postImg,likes,timespamp,id})=> {
           <BookmarkBorderIcon/>
         </div>
       </div>
-      Liked by {likes} people
+      <div>{postTitle}</div>
+      Liked by {likesCount} people
      </div>
     </div>
   )
