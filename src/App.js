3f32041e0f07f75ab0ee9d788d22fba06fbb829a
mehraneh <139059903+mehranehsoleimani66@users.noreply.github.com>
2023-09-24 16:33:19 +0330

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,66 @@
-
-import { Route,Routes } from 'react-router';
 import './App.css';
 import HomePage from './HomePage';
-import LogIn from './login/LogIn';
+import { createContext, useEffect, useState } from 'react';
+import Authontication from './authontication/Authontication';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 
+
+
+export const userContext = createContext()
 function App() {
+
+  const [user,setUser]=useState({})
+
+
+  useEffect(()=>{
+    console.log (user,'user dideh shod')
+  },[])
+  
+  useEffect(()=>{
+    
+    if (user?.token){
+     localStorage.setItem('user',JSON.stringify(user))
+    }
+    else{
+      localStorage.setItem('user',JSON.stringify([]))
+    }
+  },[user])
+  
+  
+  
+  
+  useEffect(()=>{
+    const prevUser = JSON.parse( localStorage.getItem('user'))
+    if (prevUser){
+      setUser(prevUser)
+    }
+    else {
+      setUser([])
+    }
+    
+  }
+   ,[])
+   const setUserHandler=(item)=>{
+    setUser(item)
+    
+
+   }
+
+  
   return (
     <div className='App'>
-      <LogIn/>
-{/* <Routes>
-     <Route index element={<HomePage/>} />
-     <Route path='/Shop' element={<LogIn />}/>
-     
-   
+      <userContext.Provider value={{user,setUserHandler}}>
+      <Router>
+      <Routes>
+        <Route path="/authontication" element={<Authontication />} />
+        <Route path="/homepage" element={<HomePage />} />
       </Routes>
-       */}
+    </Router>
+     
+      
+      </userContext.Provider>
+
     
       
     </div>
