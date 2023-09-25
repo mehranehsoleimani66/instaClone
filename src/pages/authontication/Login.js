8f3a1c7372952877f4ca4e436b806f7af62cea98
mehraneh <139059903+mehranehsoleimani66@users.noreply.github.com>
2023-09-25 22:22:43 +0330

    add  authontication components in pages

---
+++ src/pages/authontication/Login.js
@@ -0,0 +1,60 @@
+import React, { useContext, useState } from "react";
+
+import "./LogIn.css";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import UserContext from "../../context/userContext";
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [user, setUser] = useState("");
+  const [password, setPassword] = useState("");
+
+  const myUserContext = useContext(UserContext);
+  const navigate = useNavigate();
+
+  const loginHandler = () => {
+    //شماره کاربر و کد تایید به بک اند ارسال  می کنیم
+    axios
+      .post("http://localhost:3000/user?user", {
+        user: user,
+        email: email,
+        password: password
+      })
+      .then(({ data }) => {
+        console.log(data, "data from post");
+        myUserContext.setUserHandler(data);
+        console.log(myUserContext, "data context");
+        navigate("/HomePage");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  return (
+    <div className="login">
+      <img src="./images.png" />
+      <input
+        type="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        placeholder="Email"
+      />
+      <input
+        type="text"
+        value={user}
+        onChange={(e) => setUser(e.target.value)}
+        placeholder="Username"
+      />
+      <input
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        placeholder="Password"
+      />
+      <button onClick={loginHandler}>Log in</button>
+    </div>
+  );
+};
+
+export default Login;
