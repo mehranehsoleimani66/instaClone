5489b661f99f9af4aded058308e00f4b1b93b04d
mehraneh <139059903+mehranehsoleimani66@users.noreply.github.com>
2023-09-28 18:09:29 +0330

    Update Suggestion.js

--- src/pages/homePage/timeLine/Suggestion.js
+++ src/pages/homePage/timeLine/Suggestion.js
@@ -1,47 +1,49 @@
-import React, { useEffect, useState } from 'react'
-import './suggestion.css'
-import { Avatar } from '@mui/material'
-import axios from 'axios'
+import React, { useEffect, useState } from "react";
+import "./suggestion.css";
+import { Avatar } from "@mui/material";
+import axios from "axios";
 
 function Suggestion() {
-
-  
-  const [suggestion,setSuggestion]=useState([])
-  const getSuggestionHandler =async()=>{
-  
-    const response = await axios.get('http://localhost:3000/posts')
-    const {data} = response
-    setSuggestion(data)
+  const [suggestion, setSuggestion] = useState([]);
+  const getSuggestionHandler = async () => {
+    const response = await axios.get("http://localhost:3000/posts");
+    const { data } = response;
+    setSuggestion(data);
     // console.log(data,'data')
- }
+  };
 
- 
-    useEffect(()=>{getSuggestionHandler();}
-    ,[])
+  useEffect(() => {
+    getSuggestionHandler();
+  }, []);
 
-  
   return (
+    <div className="suggestion">
+      <div className="suggestion_title">Suggestion for you</div>
 
-    <div className='suggestion'>
-<div className='suggestion_title'>Suggestion for you
-</div>
-      
-{suggestion.map((item)=>(
-
-  <div className='suggestion_users' key={item.postId}>
-  <div className='suggestion_usernameLeft'>
-    <span className='avatar'>
-    <img src={item.userAvatar} style={{width:'50px', height:'50px', borderRadius:'50px',paddingRight:'5px'}} alt=""/>
-    </span>
-    <div className='username_info'>
-    <span className='username'>{item.userName}</span>
-    <span className='relation'>new in instagram</span>
+      {suggestion.map((item) => (
+        <div className="suggestion_users" key={item.postId}>
+          <div className="suggestion_usernameLeft">
+            <span className="avatar">
+              <img
+                src={item.userAvatar}
+                style={{
+                  width: "50px",
+                  height: "50px",
+                  borderRadius: "50px",
+                  paddingRight: "5px"
+                }}
+                alt=""
+              />
+            </span>
+            <div className="username_info">
+              <span className="username">{item.userName}</span>
+              <span className="relation">new in instagram</span>
+            </div>
+          </div>
+          <button className="follow_button">Follow</button>
+        </div>
+      ))}
     </div>
-  </div>
-<button className='follow_button'>Follow</button>
-  
-    </div>
-))}
-</div>
-)}
-export default Suggestion
+  );
+}
+export default Suggestion;
