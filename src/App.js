54c48f3886bbdfa43035eadd6d0c5a73cd32ea7e
mehraneh <139059903+mehranehsoleimani66@users.noreply.github.com>
2023-09-28 18:08:15 +0330

    update app.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import { UserProvider } from "./context/userContext";
 
 // library
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// pages
+// // pages
 
 import Authontication from "./pages/authontication/Authontication";
 import HomePage from "./pages/homePage/HomePage";
@@ -17,46 +17,58 @@ import HomePage from "./pages/homePage/HomePage";
 //create useContext
 
 function App() {
-  // localState
-  const [userName, setUserName] = useState("");
-  const [userEmail, setUserEmail] = useState("");
-  const [userPassword, setUserPassword] = useState("");
+  //   // localState
+  //   const [userName, setUserName] = useState("");
+  //   const [userEmail, setUserEmail] = useState("");
+  //   const [userPassword, setUserPassword] = useState("");
 
-  //get the user from the localStorage
-  useEffect(() => {
-    const storedUserName = JSON.parse(localStorage.getItem("userName"));
-    const storedUserEmail = JSON.parse(localStorage.getItem("userEmail"));
-    const storedUserPassword = JSON.parse(localStorage.getItem("userPassword"));
-    const doesUserExist =
-      !!storedUserName && !!storedUserEmail && !!storedUserPassword;
-    if (doesUserExist) {
-      setUserName(storedUserName);
-      setUserEmail(storedUserEmail);
-      setUserPassword(storedUserPassword);
-    }
-  }, []);
+  //   //get the user from the localStorage
+  //   useEffect(() => {
+  //     const storedUserName = JSON.parse(localStorage.getItem("userName"));
+  //     const storedUserEmail = JSON.parse(localStorage.getItem("userEmail"));
+  //     const storedUserPassword = JSON.parse(localStorage.getItem("userPassword"));
+  //     const doesUserExist =
+  //       !!storedUserName && !!storedUserEmail && !!storedUserPassword;
+  //     if (doesUserExist) {
+  //       setUserName(storedUserName);
+  //       setUserEmail(storedUserEmail);
+  //       setUserPassword(storedUserPassword);
+  //     }
+  //   }, []);
 
-  const setUserHandler = (userName, userEmail, userPassword) => {
-    setUserName(userName);
-    setUserEmail(userEmail);
-    setUserPassword(userPassword);
-    localStorage.setItem("userName", userName);
-    localStorage.setItem("userEmail", userEmail);
-    localStorage.setItem("userPassword", userPassword);
-  };
+  //   const setUserHandler = (
+  //     storedUserName,
+  //     storedUserEmail,
+  //     storedUserPassword
+  //   ) => {
+  //     setUserName(userName);
+  //     setUserEmail(userEmail);
+  //     setUserPassword(userPassword);
+  //     localStorage.setItem("userName", JSON.stringify(storedUserName));
+  //     localStorage.setItem("userEmail", JSON.stringify(storedUserEmail));
+  //     localStorage.setItem("userPassword", JSON.stringify(storedUserPassword));
+  //   };
 
   return (
     <div className="App">
-      <UserProvider
-        value={{ userName, userEmail, userPassword, setUserHandler }}
-      >
-        <Router>
-          <Routes>
-            <Route path="/" element={<Authontication />} />
-            <Route path="/homepage" element={<HomePage />} />
-          </Routes>
-        </Router>
-      </UserProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Authontication />} />
+          <Route path="/homepage" element={<HomePage />} />
+        </Routes>
+      </Router>
+      {/* <>
+        <UserProvider
+          value={{ userName, userEmail, userPassword, setUserHandler }}
+        >
+          <Router>
+            <Routes>
+              <Route path="/" element={<Authontication />} />
+              <Route path="/homepage" element={<HomePage />} />
+            </Routes>
+          </Router>
+        </UserProvider>
+      </> */}
     </div>
   );
 }
