a8b66532901d9a3bfa8b5aebf6119182e1f257b0
mehraneh <139059903+mehranehsoleimani66@users.noreply.github.com>
2023-09-28 18:08:36 +0330

    Update Authontication.js

--- src/pages/authontication/Authontication.js
+++ src/pages/authontication/Authontication.js
@@ -1,23 +1,42 @@
-import React from 'react'
-import './authontication.css'
-import Login from './Login'
+import React, { useState } from "react";
+import "./authontication.css";
+import Login from "./Login";
+import Signup from "./Signup";
 
 function Authontication() {
+  const [active, setActive] = useState("login");
+  const handelChange = () => {
+    setActive(active === "login" ? "signup" : "login");
+  };
+
   return (
-    <div className='authontication'>
-        <div className='auth_left'>
-            <img src="https://static.cdninstagram.com/images/instagram/xig/homepage/phones/home-phones.png?__makehaste_cache_breaker=HOgRclNOosk" alt=''/>
+    <div className="authontication">
+      <div className="auth_left">
+        <img
+          src="https://static.cdninstagram.com/images/instagram/xig/homepage/phones/home-phones.png?__makehaste_cache_breaker=HOgRclNOosk"
+          alt=""
+        />
+      </div>
+      <div className="auth_right">
+        {active === "login" ? <Login /> : <Signup />}
+        <div className="auth_more">
+          <span>
+            {active === "login" ? (
+              <>
+                Do'nt You Have Account ?
+                <button onClick={handelChange}>Sign up</button>
+              </>
+            ) : (
+              <>
+                Do You Have Account ?
+                <button onClick={handelChange}>log in</button>
+              </>
+            )}
+          </span>
         </div>
-        <div className='auth_right'>
-            <Login/>
-            <div className='auth_more'>
-                <span>Do You Have Account ?
-                    <button>Sign up</button>
-                </span>
-            </div>
-        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Authontication
+export default Authontication;
