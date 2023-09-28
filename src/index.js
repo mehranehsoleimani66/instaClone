d0fc2f5cc324d5c88315aa7ff919fee937e75ea0
mehraneh <139059903+mehranehsoleimani66@users.noreply.github.com>
2023-09-28 18:08:31 +0330

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import ReactDOM from "react-dom/client";
 
 import App from "./App";
 
+import { AuthProvider } from "./context/userContext";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <AuthProvider>
     <App />
-  </React.StrictMode>
+  </AuthProvider>
 );
