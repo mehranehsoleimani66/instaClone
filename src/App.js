2a19d5b5a6ecf18bf6d1bf7f391ff22a6731fdcc
mehraneh <139059903+mehranehsoleimani66@users.noreply.github.com>
2023-10-01 18:26:27 +0330

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,47 +1,28 @@
 // style
 import "./App.css";
 
-// hooks
-import { useEffect, useState } from "react";
-
-// context
-import { UserProvider } from "./context/userContext";
-
 // library
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 // // pages
 
 import Authontication from "./pages/authontication/Authontication";
 import HomePage from "./pages/homePage/HomePage";
+// import { AuthCountext } from "./context/AuthContext";
 
 //create useContext
 
 function App() {
-  //   // localState
-  const [userName, setUserName] = useState("");
-  const [userEmail, setUserEmail] = useState("");
-  const [userPassword, setUserPassword] = useState("");
-
+  // const { userEmail, userPassword, setUserEmail, setUserPassword } =
+  //   AuthCountext();
   return (
     <div className="App">
       <>
-        <UserProvider
-          value={{
-            userName,
-            userEmail,
-            userPassword,
-            setUserName,
-            setUserEmail,
-            setUserPassword
-          }}
-        >
-          <Router>
-            <Routes>
-              <Route path="/" element={<Authontication />} />
-              <Route path="/homepage" element={<HomePage />} />
-            </Routes>
-          </Router>
-        </UserProvider>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Authontication />} />
+            <Route path="/homepage" element={<HomePage />} />
+          </Routes>
+        </Router>
       </>
     </div>
   );
