e92265c7e6c0b788871abd2e40a0d2527629f076
mehraneh <139059903+mehranehsoleimani66@users.noreply.github.com>
2023-10-01 18:26:54 +0330

    Update HomePage.js

--- src/pages/homePage/HomePage.js
+++ src/pages/homePage/HomePage.js
@@ -1,19 +1,19 @@
-import React from 'react'
-import './HomePage.css';
-import SideBar from '../homePage/navigation/SideBar';
-import TimeLine from '../homePage/timeLine/TimeLine';
+import React from "react";
+import "./HomePage.css";
+import SideBar from "../homePage/navigation/SideBar";
+import TimeLine from "../homePage/timeLine/TimeLine";
 
 function HomePage() {
   return (
-    <div className='homepage'>
-      <div className='homepage_nav'>
-        <SideBar/>
+    <div className="homepage">
+      <div className="homepage_nav">
+        <SideBar />
       </div>
-      <div className='homepage_timeline'>
-        <TimeLine/>
+      <div className="homepage_timeline">
+        <TimeLine />
       </div>
     </div>
-  )
+  );
 }
 
 export default HomePage;
