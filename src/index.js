747f3c9ef4d1ce4a159f4b0d29898a14dfaf5a5d
mehraneh <139059903+mehranehsoleimani66@users.noreply.github.com>
2023-10-04 00:14:35 +0330

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { AuthProvider } from "./context/AuthContext";
 
+import "react-toastify/dist/ReactToastify.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <AuthProvider>
