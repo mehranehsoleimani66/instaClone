7b6ef22c33723202ba73ce2887323560e07ea3f9
mehraneh <139059903+mehranehsoleimani66@users.noreply.github.com>
2023-10-04 08:59:12 +0330

    Create TimeLine.js

---
+++ src/pages/homePage/components/timeLine/TimeLine.js
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from "react";
+import "./timeline.css";
+import Suggestion from "../Suggestion";
+import axios from "axios";
+import Post from "./Post";
+const TimeLine = () => {
+  const [posts, setPost] = useState([]);
+  const getPostsHandler = async () => {
+    const response = await axios.get("http://localhost:3000/posts");
+    const { data } = response;
+    setPost(data);
+    // console.log(data,'data')
+  };
+
+  useEffect(() => {
+    getPostsHandler();
+  }, []);
+
+  return (
+    <div className="timeline">
+      <div className="timeline_left">
+        <div className="timeline_post">
+          {posts.map((item) => (
+            <Post
+              postId={item.postId}
+              userName={item.userName}
+              userAvatar={item.userAvatar}
+              likesCount={item.likesCount}
+              commentText={item.commentText}
+              postImage={item.postImage}
+              postTitle={item.postTitle}
+              postBody={item.postBody}
+              timespamp={item.timespamp}
+            />
+          ))}
+        </div>
+      </div>
+      <div className="timeline_right">
+        <Suggestion />
+      </div>
+    </div>
+  );
+};
+
+export default TimeLine;
