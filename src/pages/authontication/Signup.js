cb6ccc3033ad1506b1ae5fbb120017af46c82b8f
mehraneh <139059903+mehranehsoleimani66@users.noreply.github.com>
2023-10-06 19:59:23 +0330

    Update Signup.js

--- src/pages/authontication/Signup.js
+++ src/pages/authontication/Signup.js
@@ -1,91 +1,40 @@
-import React from "react";
+import React, { useRef } from "react";
 import "./Signup.css";
 import axios from "axios";
-import { AuthCountext } from "../../context/AuthContext";
+
 import { useNavigate } from "react-router";
 import { toast } from "react-toastify";
 
 function Signup() {
-  const {
-    userEmail,
-    setUserEmail,
-    setUserPassword,
-    userPassword,
-    userName,
-    setUserName
-  } = AuthCountext();
-
   const navigate = useNavigate();
+  const inputEmailRef = useRef(null);
+  const inputPasswordRef = useRef(null);
+  const inputUsernameRef = useRef(null);
 
-  const IsValidate = () => {
-    let isproceed = true;
-    let errmsg = "please enter the value in ";
-    if (userName === null || userName === "") {
-      isproceed = false;
-      errmsg += "userName";
-    }
-    // if (userEmail === null || userEmail === "") {
-    //   isproceed = false;
-    //   errmsg += "Email";
-    // }
-    // if (userPassword === null || userPassword === "") {
-    //   isproceed = false;
-    //   errmsg += "password";
-    // }
-
-    if (!isproceed) {
-      toast.warning(errmsg);
-    }
-    // } else {
-    //   if (/^[a-zA-Z0-9]+@[a-zA-z0-9]+\.[A-Za-z]$/.test(userEmail)) {
-    //   } else {
-    //     isproceed = false;
-    //     toast.warning("please enter the valid Email");
-    //   }
-    //   console.log(isproceed);
-    // }
-    return isproceed;
-  };
-
-  const registerHandler = () => {
-    let obj = { userName, userEmail, userPassword };
-    console.log(obj);
-
-    if (IsValidate()) {
-      axios
-        .post("http://localhost:3000/users")
-        // , {
-        //   headers: { "content-type": "application/json" },
-        //   body: JSON.stringify(obj)
-        // })
-        .then((response) => {
-          toast.success("Registered Success");
-          console.log(response);
-          navigate("/login");
-        })
-        .catch((err) => {
-          toast.error("Registered faild");
-          console.log("faild");
-        });
-      // axios("http://localhost:3000/users", {
-      //   method: "POST",
-      //   headers: { "content-type": "application/json" },
-      //   body: JSON.stringify(userEmail)
-      // })
-      //   .then((response) => {
-      //     toast.success("Registered Success");
-      //     console.log(response);
-      //     navigate("/login");
-      //   })
-      //   .catch((err) => {
-      //     toast.error("Registered faild");
-      //     console.log("faild");
-      //   });
-    }
+  const registerHandler = async (e) => {
+    e.preventDefault();
+    const newUser = {
+      id: Math.random(),
+      userName: inputUsernameRef.current.value,
+      userPassword: inputPasswordRef.current.value,
+      userEmail: inputEmailRef.current.value
+    };
+    await axios
+      .post("http://localhost:3000/users", newUser)
+      .then((response) => {
+        toast.success("Registered Success");
+        console.log(response);
+        navigate("/login");
+      })
+      .catch((err) => {
+        console.log(err);
+        toast.error("Registered faild");
+        console.log("faild");
+      });
   };
 
   return (
-    <div className="signup-page">
+    <form className="signup-page" onSubmit={registerHandler}>
       <div className="sign_left">
         <img
           src="https://static.cdninstagram.com/images/instagram/xig/homepage/phones/home-phones.png?__makehaste_cache_breaker=HOgRclNOosk"
@@ -98,32 +47,19 @@ function Signup() {
           <label>
             User Name<span className="errmsg">*</span>
           </label>
-          <input
-            value={userName}
-            type="text"
-            onChange={(e) => setUserName(e.target.value)}
-            placeholder="Username"
-          />
+          <input type="text" placeholder="Username" ref={inputUsernameRef} />
 
           <label>
             User Email<span className="errmsg">*</span>
           </label>
-          <input
-            value={userEmail}
-            type="email"
-            onChange={(e) => setUserEmail(e.target.value)}
-          />
+          <input ref={inputEmailRef} />
 
           <label>
             User Password<span className="errmsg">*</span>
           </label>
-          <input
-            value={userPassword}
-            type="password"
-            onChange={(e) => setUserPassword(e.target.value)}
-          />
+          <input type="password" ref={inputPasswordRef} />
 
-          <button onClick={registerHandler}>sign up</button>
+          <button>sign up</button>
         </div>
 
         <div className="sign_more">
@@ -132,7 +68,7 @@ function Signup() {
           </span>
         </div>
       </div>
-    </div>
+    </form>
   );
 }
 
