ae73acb2a356d8a1f6f614919612c993f177203e
mehraneh <139059903+mehranehsoleimani66@users.noreply.github.com>
2023-10-10 14:21:11 +0330

    updated

---
+++ src/pages/homePage/SideBar.jsx
@@ -0,0 +1,102 @@
+// import style
+import './SideBar.css'
+//import ants library
+import { message } from 'antd'
+
+import React, { useEffect } from 'react'
+
+import { Link, useNavigate } from 'react-router-dom'
+//import custom hook
+import { AuthCountext } from '../../context/AuthContext'
+
+//import mui liabrary
+import { Avatar } from '@mui/material'
+import ChatIcon from '@mui/icons-material/Chat'
+import MenuIcon from '@mui/icons-material/Menu'
+import SearchIcon from '@mui/icons-material/Search'
+import ExploreIcon from '@mui/icons-material/Explore'
+import SlideshowIcon from '@mui/icons-material/Slideshow'
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder'
+import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline'
+
+function SideBar() {
+  const { user, setUser } = AuthCountext()
+
+  const navigate = useNavigate()
+  const logOutHandler = () => {
+    localStorage.clear()
+
+    setUser('')
+    message.success('خارج شدید')
+    navigate('/login')
+  }
+  useEffect(() => {
+    const user = JSON.parse(localStorage.getItem('myArray'))
+    setUser(user)
+  }, [])
+
+  return (
+    <div className='sidebar'>
+      <img className='sidebar_logo' src='./images/logo.png' alt='' />
+      <div className='sidebar-buttons'>
+        <Link to='/profile' style={{ textDecoration: 'none' }}>
+          <button className='sidebar_button'>
+            {user[0].userName ? (
+              <div className='user__email'>
+                <Avatar
+                  style={{
+                    border: '3px solid green'
+                  }}
+                  src='./images/profile.png'
+                ></Avatar>
+                <span className='email__text'>{user[0].userName}</span>
+              </div>
+            ) : null}
+          </button>
+        </Link>
+
+        <button className='sidebar_button'>
+          <SearchIcon />
+          <span>Search</span>
+        </button>
+        <button className='sidebar_button'>
+          <ExploreIcon />
+          <span>Explore</span>
+        </button>
+        <button className='sidebar_button'>
+          <SlideshowIcon />
+          <span>Reels</span>
+        </button>
+        <button className='sidebar_button'>
+          <ChatIcon />
+          <span>Messages</span>
+        </button>
+        <button className='sidebar_button'>
+          <FavoriteBorderIcon />
+          <span>Notification</span>
+        </button>
+        <button className='sidebar_button'>
+          <AddCircleOutlineIcon />
+          <span>Create</span>
+        </button>
+        <div className='user__info '>
+          {user ? (
+            <div>
+              <button className='logout__button' onClick={logOutHandler}>
+                logout
+              </button>
+            </div>
+          ) : null}
+        </div>
+        <div className='sidebar_more'>
+          <button>
+            <MenuIcon />
+            <span>More</span>
+          </button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default SideBar
---
+++ src/pages/homePage/Suggestion.jsx
@@ -0,0 +1,49 @@
+import './Suggestion.css'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+
+function Suggestion() {
+  const [suggestion, setSuggestion] = useState([])
+
+  const getSuggestionHandler = async () => {
+    const response = await axios.get('http://localhost:3000/posts')
+    const { data } = response
+    setSuggestion(data)
+  }
+
+  useEffect(() => {
+    getSuggestionHandler()
+  }, [])
+
+  return (
+    <div className='suggestion'>
+      <div className='suggestion_title'>Suggestion for you</div>
+
+      {suggestion.map((item) => (
+        <div className='suggestion_users' key={item.postId}>
+          <div className='suggestion_usernameLeft'>
+            <span className='avatar'>
+              <img
+                src={item.userAvatar}
+                style={{
+                  width: '50px',
+                  height: '50px',
+                  borderRadius: '50px',
+                  paddingRight: '5px',
+                  border: '3px solid green'
+                }}
+                alt=''
+              />
+            </span>
+            <div className='username_info'>
+              <span className='username'>{item.userName}</span>
+              <span className='relation'>new in instagram</span>
+            </div>
+          </div>
+          <button className='follow_button'>Follow</button>
+        </div>
+      ))}
+    </div>
+  )
+}
+export default Suggestion
