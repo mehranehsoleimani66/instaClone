51f3e9677a735c6d0b2a9d210612b0bbdaaa42a5
mehraneh <139059903+mehranehsoleimani66@users.noreply.github.com>
2023-10-10 14:19:10 +0330

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,31 @@
-// style
-import "./App.css";
-
+import React from 'react'
+import './App.css'
 // library
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { ToastContainer } from "react-toastify";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
 // // pages
 
-import HomePage from "./pages/homePage/HomePage";
-import Signup from "./pages/authontication/Signup";
-import Login from "./pages/authontication/Login";
-import Profile from "./pages/profile/Profile";
+import HomePage from './pages/homePage/HomePage'
+import Signup from './pages/authentication/Signup'
+import Login from './pages/authentication/Login'
+import Profile from './pages/profile/Profile'
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       <>
-        <ToastContainer theme="colored"></ToastContainer>
+        <ToastContainer theme='colored'></ToastContainer>
         <Router>
           <Routes>
-            <Route path="/" element={<Signup />} />
-            <Route path="/profile" element={<Profile />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/homepage" element={<HomePage />} />
+            <Route path='/' element={<Signup />} />
+            <Route path='/profile' element={<Profile />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/homepage' element={<HomePage />} />
           </Routes>
         </Router>
       </>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
