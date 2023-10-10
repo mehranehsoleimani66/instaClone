f8c833bc828a33c193e74a502aaee7fdea3dc610
mehraneh <139059903+mehranehsoleimani66@users.noreply.github.com>
2023-10-10 12:09:08 +0330

    changed the file extension from JS to JSX

---
+++ src/pages/homePage/components/postsSection/Posts.jsx
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from "react";
+import "./Posts.css";
+import Suggestion from "../Suggestion";
+import axios from "axios";
+import Post from "./Post";
+
+// create timeline component for get Post component's data from database
+const Posts = () => {
+  const [posts, setPost] = useState([]);
+  const getPostsHandler = async () => {
+    const response = await axios.get("http://localhost:3000/posts");
+    const { data } = response;
+    setPost(data);
+  };
+
+  useEffect(() => {
+    getPostsHandler();
+  }, []);
+
+  return (
+    <div className="timeline">
+      <div className="timeline_left">
+        <div className="timeline_post">
+          {posts.map((item) => (
+            <Post
+              postId={item.postId}
+              userName={item.userName}
+              userAvatar={item.userAvatar}
+              likesCount={item.likesCount}
+              commentText={item.commentText}
+              postImage={item.postImage}
+              postTitle={item.postTitle}
+              postBody={item.postBody}
+              timespamp={item.timespamp}
+            />
+          ))}
+        </div>
+      </div>
+      <div className="timeline_right">
+        <Suggestion />
+      </div>
+    </div>
+  );
+};
+
+export default Posts;
