d57803720006d5d8400af1ccc4bc0e66b5c1f25c
mehraneh <139059903+mehranehsoleimani66@users.noreply.github.com>
2023-10-12 15:36:33 +0330

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,21 +9,30 @@ import HomePage from './pages/homePage/HomePage'
 import Signup from './pages/authentication/Signup'
 import Login from './pages/authentication/Login'
 import Profile from './pages/profile/Profile'
-
+import { useThemeContext } from './context/ThemeContext'
+import PowerSettingsNewIcon from '@mui/icons-material/PowerSettingsNew'
 function App() {
+  const theme = useThemeContext()
+  const darkMode = theme.darkMode
+
+  const modeHandler = () => {
+    theme.setDarkMode(!darkMode)
+  }
   return (
-    <div className='App'>
-      <>
-        <ToastContainer theme='colored'></ToastContainer>
-        <Router>
-          <Routes>
-            <Route path='/' element={<Signup />} />
-            <Route path='/profile' element={<Profile />} />
-            <Route path='/login' element={<Login />} />
-            <Route path='/homepage' element={<HomePage />} />
-          </Routes>
-        </Router>
-      </>
+    <div className={`${darkMode ? 'App-dark' : 'App-light'}`}>
+      <button onClick={modeHandler} className={`${darkMode ? 'btn-light' : 'btn-dark'}`} style={{ float: 'right', margin: '10px' }}>
+        <PowerSettingsNewIcon></PowerSettingsNewIcon>
+        {/* {darkMode ? 'تاریک' : 'روشن'} */}
+      </button>
+      <ToastContainer theme='colored'></ToastContainer>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Signup />} />
+          <Route path='/profile' element={<Profile />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/homepage' element={<HomePage />} />
+        </Routes>
+      </Router>
     </div>
   )
 }
