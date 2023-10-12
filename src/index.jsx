f5d0125fdb02fec0ca2631a6da054cee4396d7a9
mehraneh <139059903+mehranehsoleimani66@users.noreply.github.com>
2023-10-12 15:36:49 +0330

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import { AuthProvider } from "./context/AuthContext";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import { AuthProvider } from './context/AuthContext'
+import 'react-toastify/dist/ReactToastify.css'
+import { ThemeProvider } from './context/ThemeContext'
 
-import "react-toastify/dist/ReactToastify.css";
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <AuthProvider>
-    <App />
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
   </AuthProvider>
-);
+)
