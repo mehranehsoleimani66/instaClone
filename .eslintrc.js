be9b08297f662786637fc3e06b28a232dd6a756c
mehraneh <139059903+mehranehsoleimani66@users.noreply.github.com>
2023-10-12 15:35:55 +0330

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,12 +19,13 @@ module.exports = {
     node: true
   },
   extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:prettier/recommended' // Make this the last element so prettier config overrides other formatting rules
+    // 'eslint:recommended',
+    // 'plugin:react/recommended',
+    // 'plugin:prettier/recommended' // Make this the last element so prettier config overrides other formatting rules
   ],
   rules: {
-    'no-unused-vars': ['error', { vars: 'all', args: 'after-used', ignoreRestSiblings: false }],
-    'prettier/prettier': ['error', {}, { usePrettierrc: true }]
+    'prettier/prettier': 'off'
+    // 'no-unused-vars': ['error', { vars: 'all', args: 'after-used', ignoreRestSiblings: false }],
+    // 'prettier/prettier': ['error', {}, { usePrettierrc: true }]
   }
 }
